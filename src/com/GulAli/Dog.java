f15e5756d59d6c5ff512b45282e4d95d3ff296bb
Ali <a.aligul@hotmail.com>
2022-03-17 16:32:28 +0300

    v3

--- src/com/GulAli/Dog.java
+++ src/com/GulAli/Dog.java
@@ -1,6 +1,7 @@
 package com.GulAli;
 
-class Dog extends Animal {
+class Dog implements Animal {
+    public String name;
     @Override
     public void displayInfo() {
         System.out.println("I am a dog and my name is " + this.name);
--- src/com/GulAli/Main.java
+++ src/com/GulAli/Main.java
@@ -2,15 +2,12 @@ package com.GulAli;
 
 class Main {
     public static void main(String[] args) {
-        Animal a1 = new Animal();
         Dog d1 = new Dog();
         Cat c1 = new Cat();
 
-        a1.name = "Animal";
         d1.name = "Doggy";
         c1.name = "Catty";
 
-        a1.displayInfo();
         d1.displayInfo();
         c1.displayInfo();
     }
