a8a710fbbbc587b01d447b9d2abc46c9e250d435
Ali <a.aligul@hotmail.com>
2022-03-17 15:27:26 +0300

    v2

--- src/com/GulAli/Main.java
+++ src/com/GulAli/Main.java
@@ -4,12 +4,10 @@ class Main {
     public static void main(String[] args) {
         Animal a1 = new Animal();
         Dog d1 = new Dog();
-        Dog d2 = new Dog();
         Cat c1 = new Cat();
 
         a1.name = "Animal";
         d1.name = "Doggy";
-        d2.name = "Doggy 2";
         c1.name = "Catty";
 
         a1.displayInfo();
